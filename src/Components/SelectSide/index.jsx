05af0b483b094f13bef586ed3c703396e5850eff
rectangle-man <98609178+rectangle-man@users.noreply.github.com>
2023-02-15 20:12:37 +0530

    Improved UI

--- src/Components/SelectSide/index.jsx
+++ src/Components/SelectSide/index.jsx
@@ -1,8 +1,17 @@
+<<<<<<< Updated upstream
 import SelectButton from "./SelectButton";
 // import { auth } from "../../firebaseConfig.js";
 // import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import "./index.css"
 import {motion} from "framer-motion";
+=======
+
+import { auth, provider, db } from "../../firebaseConfig.js";
+import { signInWithPopup } from "firebase/auth";
+import "./index.css";
+import { doc, setDoc } from "firebase/firestore";
+import { motion } from "framer-motion";
+>>>>>>> Stashed changes
 
 const SelectSide = () => {
   async function handlePcClick() {
@@ -18,8 +27,20 @@ const SelectSide = () => {
   }
 
   return (
+<<<<<<< Updated upstream
     <motion.div animate={{ y: [-100,50] }} transition={{ type: "spring", stiffness: 400 }} className="mainContainer">
       <SelectButton image = {"./Assets/ps5.png"} label={"PC"} onClick={handlePcClick} />
+=======
+    <motion.div
+      animate={{ y: [-100, 50] }}
+      transition={{ type: "spring", stiffness: 400 }}
+      className="mainContainer"
+    >
+      <div className="selectButtonLeft" onClick={() => handleClick("PS")}>
+          <img className="buttonImageLeft" src="./Assets/ps5.png"/>
+      </div>;
+
+>>>>>>> Stashed changes
       <motion.div className="mainBody">
         <motion.div className="logoDiv">
           <img src="./Assets/tagLogo.png" className="tagLogo"/>
@@ -28,7 +49,13 @@ const SelectSide = () => {
           CHOOSE YOUR PLATFORM
         </motion.div>
       </motion.div>
+<<<<<<< Updated upstream
       <SelectButton image = {"./Assets/pc.png"} label={"PS"} />
+=======
+      <div className="selectButtonRight" onClick={() => handleClick("PC")}>
+          <img className="buttonImageRight" src="./Assets/pc.png"/>
+      </div>;
+>>>>>>> Stashed changes
     </motion.div>
   );
 };
