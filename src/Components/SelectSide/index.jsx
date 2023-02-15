dc8bb3a98f8f98f98cd1df8e5a31f3426511d81f
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-02-15 16:33:59 +0530

    Firestore Basic data

--- src/Components/SelectSide/index.jsx
+++ src/Components/SelectSide/index.jsx
@@ -1,24 +1,39 @@
 import SelectButton from "./SelectButton";
-import { auth, provider } from "../../firebaseConfig.js";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { db, auth, provider } from "../../firebaseConfig.js";
+import { signInWithPopup } from "firebase/auth";
+import { doc, setDoc } from "firebase/firestore";
 
 const SelectSide = () => {
-  async function handlePcClick() {
+  async function login() {
+    const result = await signInWithPopup(auth, provider);
+    return result.user;
+  }
+
+  async function handleClick(platform) {
+    let user;
     try {
-      const result = await signInWithPopup(auth, provider);
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      console.log(credential);
-      const user = result.user;
-      console.log(user);
+      user = await login();
     } catch (e) {
-      console.log(e);
+      console.log("Error while logging in: ", e);
+    }
+    try {
+      const docRef = await setDoc(doc(db, "users", user.email), {
+        platform,
+      });
+      // const docRef = await addDoc(collection(db, "users"), {
+      //   id: user.email,
+      //   platform,
+      // });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
     }
   }
 
   return (
     <div>
-      <SelectButton label={"PC"} onClick={handlePcClick} />
-      <SelectButton label={"PS"} />
+      <SelectButton label={"PC"} onClick={() => handleClick("PC")} />
+      <SelectButton label={"PS"} onClick={() => handleClick("PS")} />
     </div>
   );
 };
