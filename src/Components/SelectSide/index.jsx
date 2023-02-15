5f737e9f3eb6fed6ee1c64c3914fc4227b655b7c
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-02-15 15:37:51 +0530

    Google Login bugs Fixed

--- src/Components/SelectSide/index.jsx
+++ src/Components/SelectSide/index.jsx
@@ -1,11 +1,11 @@
 import SelectButton from "./SelectButton";
-import { auth } from "../../firebaseConfig.js";
+import { auth, provider } from "../../firebaseConfig.js";
 import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 const SelectSide = () => {
   async function handlePcClick() {
     try {
-      const result = await signInWithPopup(auth, GoogleAuthProvider());
+      const result = await signInWithPopup(auth, provider);
       const credential = GoogleAuthProvider.credentialFromResult(result);
       console.log(credential);
       const user = result.user;
