6a277f5a869bf7d06f8c34f1cf134a4bef57ae30
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-02-15 16:45:10 +0530

    Merge Conflicts

--- src/Components/SelectSide/index.jsx
+++ src/Components/SelectSide/index.jsx
@@ -1,11 +1,11 @@
 import SelectButton from "./SelectButton";
-import { auth } from "../../firebaseConfig.js";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
-import "./index.css"
-import {motion} from "framer-motion";
+import { auth, provider, db } from "../../firebaseConfig.js";
+import { signInWithPopup } from "firebase/auth";
+import "./index.css";
+import { doc, setDoc } from "firebase/firestore";
+import { motion } from "framer-motion";
 
 const SelectSide = () => {
-
   async function login() {
     const result = await signInWithPopup(auth, provider);
     return result.user;
@@ -22,29 +22,42 @@ const SelectSide = () => {
       const docRef = await setDoc(doc(db, "users", user.email), {
         platform,
       });
-      // const docRef = await addDoc(collection(db, "users"), {
-      //   id: user.email,
-      //   platform,
-      // });
       console.log("Document written with ID: ", docRef.id);
     } catch (e) {
       console.error("Error adding document: ", e);
     }
   }
-  
+
   return (
-    <motion.div animate={{ y: [-100,50] }} transition={{ type: "spring", stiffness: 400 }} className="mainContainer">
-      <SelectButton image = {"./Assets/ps5.png"} label={"PC"} onClick={() => handleClick("PC")} />
+    <motion.div
+      animate={{ y: [-100, 50] }}
+      transition={{ type: "spring", stiffness: 400 }}
+      className="mainContainer"
+    >
+      <SelectButton
+        image={"./Assets/ps5.png"}
+        label={"PC"}
+        onClick={() => handleClick("PC")}
+      />
       <motion.div className="mainBody">
         <motion.div className="logoDiv">
-          <img src="./Assets/tagLogo.png" className="tagLogo"/>
+          <img src="./Assets/tagLogo.png" className="tagLogo" alt="LOGO" />
         </motion.div>
-        <motion.div animate={{ y: -50 }} transition={{ type: "spring", stiffness: 200 }}className="mainText">
+        <motion.div
+          animate={{ y: -50 }}
+          transition={{ type: "spring", stiffness: 200 }}
+          className="mainText"
+        >
           CHOOSE YOUR PLATFORM
         </motion.div>
       </motion.div>
-      <SelectButton image = {"./Assets/pc.png"} label={"PS"} onClick={() => handleClick("PS")}/>
+      <SelectButton
+        image={"./Assets/pc.png"}
+        label={"PS"}
+        onClick={() => handleClick("PS")}
+      />
     </motion.div>
+  );
 };
 
 export default SelectSide;
