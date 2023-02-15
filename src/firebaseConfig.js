3d50b9bc48549144964a1bf8eeb80db3243fc8ea
Naman Agrawal <a6.namanagrawal@gmail.com>
2023-02-15 15:02:18 +0530

    Config: Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCz3JfwTAytuCKDu1rYDJg55uGqTStzOzA",
+  authDomain: "arcade-slot-booking.firebaseapp.com",
+  projectId: "arcade-slot-booking",
+  storageBucket: "arcade-slot-booking.appspot.com",
+  messagingSenderId: "385188826990",
+  appId: "1:385188826990:web:8d613df628e2841a66bb7f",
+  measurementId: "G-6RKP9DXM9C",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
