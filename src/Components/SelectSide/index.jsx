0d327e46e125a57865ad450b880db733342e0bde
rectangle-man <98609178+rectangle-man@users.noreply.github.com>
2023-02-15 16:36:56 +0530

    Basic UI + Animations

--- src/Components/SelectSide/index.jsx
+++ src/Components/SelectSide/index.jsx
@@ -1,25 +1,35 @@
 import SelectButton from "./SelectButton";
-import { auth } from "../../firebaseConfig.js";
-import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+// import { auth } from "../../firebaseConfig.js";
+// import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import "./index.css"
+import {motion} from "framer-motion";
 
 const SelectSide = () => {
   async function handlePcClick() {
-    try {
-      const result = await signInWithPopup(auth, GoogleAuthProvider());
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      console.log(credential);
-      const user = result.user;
-      console.log(user);
-    } catch (e) {
-      console.log(e);
-    }
+    // try {
+    //   const result = await signInWithPopup(auth, GoogleAuthProvider());
+    //   const credential = GoogleAuthProvider.credentialFromResult(result);
+    //   console.log(credential);
+    //   const user = result.user;
+    //   console.log(user);
+    // } catch (e) {
+    //   console.log(e);
+    // }
   }
 
   return (
-    <div>
-      <SelectButton label={"PC"} onClick={handlePcClick} />
-      <SelectButton label={"PS"} />
-    </div>
+    <motion.div animate={{ y: [-100,50] }} transition={{ type: "spring", stiffness: 400 }} className="mainContainer">
+      <SelectButton image = {"./Assets/ps5.png"} label={"PC"} onClick={handlePcClick} />
+      <motion.div className="mainBody">
+        <motion.div className="logoDiv">
+          <img src="./Assets/tagLogo.png" className="tagLogo"/>
+        </motion.div>
+        <motion.div animate={{ y: -50 }} transition={{ type: "spring", stiffness: 200 }}className="mainText">
+          CHOOSE YOUR PLATFORM
+        </motion.div>
+      </motion.div>
+      <SelectButton image = {"./Assets/pc.png"} label={"PS"} />
+    </motion.div>
   );
 };
 
