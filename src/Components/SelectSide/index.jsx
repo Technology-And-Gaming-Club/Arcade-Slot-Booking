28037bda6d95bf44e16c088f4f01bc17239cb478
rectangle-man <98609178+rectangle-man@users.noreply.github.com>
2023-02-17 23:05:28 +0530

    Updated UI

--- src/Components/SelectSide/index.jsx
+++ src/Components/SelectSide/index.jsx
@@ -1,50 +1,60 @@
 // import { auth } from "../../firebaseConfig.js";
-// import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import "./index.css"
-import {motion} from "framer-motion";r
+// import {motion} from "framer-motion";
 import { auth, provider, db } from "../../firebaseConfig.js";
-import { signInWithPopup } from "firebase/auth";
+// import { signInWithPopup } from "firebase/auth";
 import "./index.css";
 import { doc, setDoc } from "firebase/firestore";
 import { motion } from "framer-motion";
 
+export default function SelectSide() 
+{
 
-const SelectSide = () => {
   async function login() {
     const result = await signInWithPopup(auth, provider);
     return result.user;
   }
 
-  async function handleClick(platform) {
-    let user;
-    try {
-      user = await login();
-    } catch (e) {
-      console.log("Error while logging in: ", e);
+  const SelectSide = () =>
+  {
+    async function login()
+    {
+      const result = await signInWithPopup(auth, provider);
+      return result.user;
     }
-    try {
-      const docRef = await setDoc(doc(db, "users", user.email), {
+  } 
+  async function handleClick(platform)
+  {
+      let user;
+      try
+      {
+        user = await login();
+      } 
+      catch (e)
+      {
+      console.log("Error while logging in: ", e);
+      }
+      try
+      {
+        const docRef = await setDoc(doc(db, "users", user.email), {
         platform,
       });
       console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
+      } 
+      catch (e)
+      {
+        console.error("Error adding document: ", e);
+      }
   }
 
   return (
-
     <motion.div animate={{ y: [-100,50] }} transition={{ type: "spring", stiffness: 400 }} className="mainContainer">
 
-    <motion.div
-      animate={{ y: [-100, 50] }}
-      transition={{ type: "spring", stiffness: 400 }}
-      className="mainContainer"
-    >
+
       <div className="selectButtonLeft" onClick={() => handleClick("PS")}>
           <img className="buttonImageLeft" src="./Assets/ps5.png"/>
       </div>;
-
 
       <motion.div className="mainBody">
         <motion.div className="logoDiv">
@@ -53,8 +63,7 @@ const SelectSide = () => {
         <motion.div
           animate={{ y: -50 }}
           transition={{ type: "spring", stiffness: 200 }}
-          className="mainText"
-        >
+          className="mainText">
           CHOOSE YOUR PLATFORM
         </motion.div>
       </motion.div>
@@ -63,9 +72,7 @@ const SelectSide = () => {
           <img className="buttonImageRight" src="./Assets/pc.png"/>
       </div>;
 
-
     </motion.div>
   );
-};
-
-export default SelectSide;
+    
+}
